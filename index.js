c93d2368058b4e7a37931ae74681ab324c0c576a
Chetan Bargal <100152637+CHAITUBARGAL@users.noreply.github.com>
2025-06-12 20:27:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import dishRoutes from "./routes/dishes.js";
-
+import Dish from "./models/Dish.js";
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
@@ -16,23 +16,18 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 app.use(express.json());
 
-// Add io to req so routes can emit events
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
-
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
-
 app.use("/api/dishes", dishRoutes);
 
-// Real-time sync simulation
-import Dish from "./models/Dish.js";
 setInterval(async () => {
   const dishes = await Dish.find();
   io.emit("dishesSync", dishes);
@@ -40,4 +35,4 @@ setInterval(async () => {
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}
+});
