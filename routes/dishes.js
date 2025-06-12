1d9f17d80bff2fceea8704b925ab7a7193981af1
Chetan Bargal <100152637+CHAITUBARGAL@users.noreply.github.com>
2025-06-12 20:23:31 +0530

    Add add dish functionality

--- routes/dishes.js
+++ routes/dishes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import Dish from "../models/Dish.js";
-import { v4 as uuidv4 } from "uuid"; // npm install uuid
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-// Get all dishes
+
 router.get("/", async (req, res) => {
   try {
     const dishes = await Dish.find();
@@ -14,7 +14,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Toggle publish status
 router.patch("/:id/toggle", async (req, res) => {
   try {
     const dish = await Dish.findOne({ dishId: req.params.id });
@@ -52,52 +51,3 @@ router.post("/", async (req, res) => {
 });
 
 export default router;
-
-// import express from "express";
-// import Dish from "../models/Dish.js";
-
-// const router = express.Router();
-
-// // Get all dishes
-// router.get("/", async (req, res) => {
-//   try {
-//     const dishes = await Dish.find();
-//     res.json(dishes);
-//   } catch (err) {
-//     res.status(500).send("Error fetching dishes");
-//   }
-// });
-
-// // Toggle publish status
-// router.patch("/:id/toggle", async (req, res) => {
-//   try {
-//     const dish = await Dish.findOne({ dishId: req.params.id });
-//     if (!dish) return res.status(404).send("Dish not found");
-//     dish.isPublished = !dish.isPublished;
-//     await dish.save();
-//     req.io.emit("dishUpdated", dish); // emit from request context
-//     res.json(dish);
-//   } catch (err) {
-//     res.status(500).send("Error toggling dish status");
-//   }
-// });
-
-// // POST: Add new dish
-// router.post("/", async (req, res) => {
-//   try {
-//     const { dishId, dishName, imageUrl, isPublished } = req.body;
-//     if (!dishId || !dishName || !imageUrl) {
-//       return res.status(400).send("Missing fields");
-//     }
-
-//     const newDish = new Dish({ dishId, dishName, imageUrl, isPublished });
-//     await newDish.save();
-
-//     req.io.emit("dishUpdated", newDish);
-//     res.status(201).json(newDish);
-//   } catch (err) {
-//     res.status(500).send("Error adding dish");
-//   }
-// });
-
-// export default router;
