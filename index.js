a4f0ce4bea61e4dc9690b65dcf62214d9a9f2844
Chetan Bargal <100152637+CHAITUBARGAL@users.noreply.github.com>
2025-06-12 20:21:40 +0530

    Update Changes

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// MongoDB connection
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
-// Routes
+
 app.use("/api/dishes", dishRoutes);
 
 // Real-time sync simulation
@@ -40,48 +40,4 @@ setInterval(async () => {
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import http from "http";
-// import { Server } from "socket.io";
-// import dotenv from "dotenv";
-// import dishRoutes from "./routes/dishes.js";
-
-// dotenv.config();
-// const PORT = process.env.PORT || 4000;
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, { cors: { origin: "*" } });
-
-// app.use(cors());
-// app.use(express.json());
-
-// // Add io to req so routes can emit events
-// app.use((req, res, next) => {
-//   req.io = io;
-//   next();
-// });
-
-// // MongoDB connection
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.error(err));
-
-// // Routes
-// app.use("/api/dishes", dishRoutes);
-
-// // Real-time sync simulation
-// import Dish from "./models/Dish.js";
-// setInterval(async () => {
-//   const dishes = await Dish.find();
-//   io.emit("dishesSync", dishes);
-// }, 1000);
-
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+}
